a52d6d641829e63f2ec0da5b90082ffebbda2fb9
RajaRaviVarma <ravi.varma@cybrilla.com>
2018-08-13 02:03:42 +0530

    Add upload feature to the application

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
+
 from sanic import Sanic
-from sanic.response import json, html
+from sanic.response import json, html, redirect
+
+UPLOAD_DIRECTORY = "./static/pictures"
 
 app = Sanic()
 
@@ -17,6 +21,17 @@ def readhtml_file(filename):
 @app.route("/")
 async def test(request):
     return json({"hello": "world"})
+
+
+@app.route("/upload", methods=["POST"])
+async def upload(request):
+    if len(request.files["picture"]) > 0:
+        picture_file = request.files["picture"][0]
+        filename = os.path.join(UPLOAD_DIRECTORY, picture_file.name)
+        with open(filename, "wb") as f:
+            f.write(picture_file.body)
+
+    return redirect(app.url_for("index"))
 
 
 @app.route("/index")
