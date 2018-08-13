23aaa3cf85a809f7d6763bc08cd6b5b10dc795b6
RajaRaviVarma <ravi.varma@cybrilla.com>
2018-08-13 22:01:07 +0530

    Store uploaded image details in database

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
+from hashlib import md5
 
+import peewee
 from sanic import Sanic
 from sanic.response import json, html, redirect
+
+from memegram.db.models import Image
 
 UPLOAD_DIRECTORY = "./static/pictures"
 
@@ -11,6 +15,20 @@ app = Sanic()
 app.static("/css", "./static/css")
 # Serve JS files
 app.static("/js", "./static/js")
+
+
+def save_image_details(image_file, image_filepath):
+    image_digest = md5(image_file.body).hexdigest()
+    try:
+        Image.create(
+            filename=image_file.name,
+            filepath=image_filepath,
+            digest=image_digest,
+        )
+        return True
+    except peewee.IntegrityError as e:
+        print("Duplicate file detected")
+        return False
 
 
 def readhtml_file(filename):
@@ -27,8 +45,10 @@ async def test(request):
 async def upload(request):
     if len(request.files["picture"]) > 0:
         picture_file = request.files["picture"][0]
-        filename = os.path.join(UPLOAD_DIRECTORY, picture_file.name)
-        with open(filename, "wb") as f:
+        filepath = os.path.join(UPLOAD_DIRECTORY, picture_file.name)
+        save_image_details(picture_file, filepath)
+
+        with open(filepath, "wb") as f:
             f.write(picture_file.body)
 
     return redirect(app.url_for("index"))
