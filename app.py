b3384dfc80c951edeb39d9d01a7d25db380292be
RajaRaviVarma <ravi.varma@cybrilla.com>
2018-08-14 00:04:47 +0530

    Render the layout to jinja2 template

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from hashlib import md5
 import peewee
 from sanic import Sanic
 from sanic.response import json, html, redirect
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from memegram.db.models import Image
 
@@ -17,6 +18,11 @@ app = Sanic()
 app.static("/css", "./static/css")
 # Serve JS files
 app.static("/js", "./static/js")
+
+jinja_env = Environment(
+    loader=FileSystemLoader(str(VIEWS_PATH)),
+    autoescape=select_autoescape(["html", "xml"]),
+)
 
 
 def save_image_details(image_file, image_filepath):
@@ -33,9 +39,11 @@ def save_image_details(image_file, image_filepath):
         return False
 
 
-def readhtml_file(filename):
-    with open(VIEWS_PATH.joinpath(filename), "r") as f:
-        return f.read()
+def render_template(filename, request):
+    return jinja_env.get_template(filename).render(
+        errors=request.get("flash", {}).get("errors", []),
+        info=request.get("flash", {}).get("info", []),
+    )
 
 
 @app.route("/")
@@ -48,17 +56,22 @@ async def upload(request):
     picture_file = request.files["picture"][0]
     if len(picture_file.name) > 0:
         filepath = os.path.join(UPLOAD_DIRECTORY, picture_file.name)
-        save_image_details(picture_file, filepath)
 
-        with open(filepath, "wb") as f:
-            f.write(picture_file.body)
+        if save_image_details(picture_file, filepath):
+            with open(filepath, "wb") as f:
+                f.write(picture_file.body)
+            request["flash"] = {"info": ["successfully saved image"]}
+        else:
+            request["flash"] = {"errors": ["Image file already exists"]}
+    else:
+        request["flash"] = {"errors": ["Please upload a file"]}
 
-    return redirect(app.url_for("index"))
+    return await index(request)
 
 
 @app.route("/index")
 async def index(request):
-    return html(readhtml_file("layout.html"))
+    return html(render_template("layout.html.jinja2", request))
 
 
 if __name__ == "__main__":
